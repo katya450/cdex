4fa4d0cbfa9af919903437dd8c7e28ad0a6b0c65
Jarno Sarvi <jarno.sarvi@reaktor.fi>
2015-01-19 17:01:44 +0200

    Changed endpoint to return “Hello world” as plain text

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,7 @@ import play.mvc.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Hello world");
     }
 
 }
