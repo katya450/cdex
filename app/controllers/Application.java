945ff78baa5fe5ec959ed77c5076c2b1f56d7d16
Jarno Sarvi <jarno.sarvi@reaktor.fi>
2015-07-08 17:11:25 +0300

    Fix intendation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,22 +13,22 @@ import models.Record;
 
 public class Application extends Controller {
 
-    public static Result create() throws Throwable {
-    	JsonNode body = request().body().asJson();
-    	if (body == null) {
-    		return badRequest("Missing JSON body");
-    	} else {
-    		Record record = Json.fromJson(body, Record.class);
-    		if (record.artist == null || record.name == null) {
-    			return badRequest("Missing fields");
-    		}
-    		record.id = Database.persist(record);
-        return ok(Json.toJson(record));
-    	}
+  public static Result create() throws Throwable {
+    JsonNode body = request().body().asJson();
+    if (body == null) {
+      return badRequest("Missing JSON body");
+    } else {
+      Record record = Json.fromJson(body, Record.class);
+      if (record.artist == null || record.name == null) {
+        return badRequest("Missing fields");
+      }
+      record.id = Database.persist(record);
+      return ok(Json.toJson(record));
     }
-    
-    public static Result getAll() throws Throwable {
-      List<Record> records = Database.getRecords();
-      return ok(Json.toJson(records));
-    }
+  }
+
+  public static Result getAll() throws Throwable {
+    List<Record> records = Database.getRecords();
+    return ok(Json.toJson(records));
+  }
 }
