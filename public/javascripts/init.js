dc197cdd3912b1dab29dcfd87ea7728fa141fc0e
Jarno Sarvi <jarno.sarvi@reaktor.fi>
2015-07-08 17:00:36 +0300

    Display all records in the UI

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,24 +1,43 @@
 $(function() {
   
-  var alert = $("#alert")
+  loadRecords()
   
   $('#add_record').click(function() {
     var newRecord = {
       artist: $('#artist').val(),
       name: $('#record_name').val()
     }
+    addRecord(newRecord)
+  })
+  
+  function addRecord(record, success) {
     $.ajax({
       type: 'POST',
       url: '/record',
       contentType: 'application/json',
       async: true,
-      data: JSON.stringify(newRecord),
-      success: recordAdded,
+      data: JSON.stringify(record),
+      success: loadRecords
     })
-  })
+  }
   
-  function recordAdded(data) {
-    alert.addClass("alert-success")
-    alert.text("Added record with id " + data.id)
+  function loadRecords() {
+    $.ajax({
+      type: 'GET',
+      url: '/records',
+      async: true,
+      success: displayRecords
+    })
+  }
+  
+  function displayRecords(records) {
+    var body = $('#record-list tbody')
+    body.html('')
+    records.forEach(function(el) {
+      body.append('<tr>' +
+          '<td>' + el.id + '</td>' +
+          '<td>' + el.artist + '</td>' +
+          '<td>' + el.name + '</td></tr>')
+    })
   }
 })
