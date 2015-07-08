90e174f24dd06e1ce1fdc4a5579b5f35b8e5d387
Jarno Sarvi <jarno.sarvi@reaktor.fi>
2015-07-08 16:59:43 +0300

    Added DAO method for getting all records

--- app/models/Database.java
+++ app/models/Database.java
@@ -7,6 +7,10 @@ import java.sql.Statement;
 
 import play.db.DB;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Database {
 
   public static int persist(Record record) throws Throwable {
@@ -22,4 +26,18 @@ public class Database {
     return keys.getInt(1);
   }
   
+  public static List<Record> getRecords() throws Throwable {
+    Connection conn = DB.getConnection();
+    Statement stm = conn.createStatement();
+    ResultSet rs = stm.executeQuery("select * from records");
+    List<Record> records = new ArrayList<Record>();
+    while (rs.next()) {
+      Record record = new Record();
+      record.artist = rs.getString("artist");
+      record.name = rs.getString("name");
+      record.id = rs.getInt("record_id");
+      records.add(record);
+    }
+    return Collections.unmodifiableList(records);
+  }
 }
