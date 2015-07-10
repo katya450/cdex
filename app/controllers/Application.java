ae42b2d28ecd6e01685031e4cb57ee88b0081d67
Katya450 <katja.lusma@gmail.com>
2015-07-10 17:37:34 +0300

    Json-pojo-json first handling.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,19 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import models.Record;
+import play.libs.Json;
 import play.mvc.*;
 
 public class Application extends Controller {
 
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result upsertRecord() {
+        JsonNode json = request().body().asJson();			//kaivaa bodyn sisällön jsonina 
+        Record record = Json.fromJson(json, Record.class);	//tsekkaa, että json vastaa record -oliota. Jos ok, muuntaa olioksi, jos ei ok, niin antaa virheen.
+        record.id = 1L;								
+        return ok(Json.toJson(record));						//ok = http200
     }
-
 }
+   
---
+++ app/models/Record.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Record {
+    public String artist;
+    public String name;
+    public Long id;
+}
