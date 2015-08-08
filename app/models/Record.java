f9f3d9890d39ad0b1da5853d57247cb1f6bc7c4e
Katya450 <katja.lusma@gmail.com>
2015-08-08 15:32:45 +0300

    Added new field "mediaType" to db and class

--- app/models/Record.java
+++ app/models/Record.java
@@ -2,16 +2,24 @@ package models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
 @Entity                                     //@ = annotaatio
+@Table(name="records")
 public class Record extends Model {
 
   private static final long serialVersionUID = 2776119609273705779L;
-
+  
+  public enum MediaType {
+      CD, DVD, LP, BD, VHS, MC
+  }
+  
   @Constraints.Required
   @Column(nullable = false)
   public String artist;
@@ -24,6 +32,9 @@ public class Record extends Model {
   @Constraints.Min(10)
   public Long id;
 
+  @Enumerated(EnumType.ORDINAL)
+  public MediaType mediaType;
+  
   public static Finder<Long, Record> find = 
       new Finder<Long, Record>(Long.class, Record.class);
 
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,11 +3,13 @@
 
 # --- !Ups
 
-create table record (
+create table records (
   id                        bigint auto_increment not null,
   artist                    varchar(255) not null,
   name                      varchar(255) not null,
-  constraint pk_record primary key (id))
+  media_type                integer,
+  constraint ck_records_media_type check (media_type in (0,1,2,3,4,5)),
+  constraint pk_records primary key (id))
 ;
 
 
@@ -17,7 +19,7 @@ create table record (
 
 SET FOREIGN_KEY_CHECKS=0;
 
-drop table record;
+drop table records;
 
 SET FOREIGN_KEY_CHECKS=1;
 
